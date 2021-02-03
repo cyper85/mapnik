46580c908cc5253ac60ae3fa7ba8dffb598f9d6b
Andreas Neumann <andr.neumann@googlemail.com>
2021-02-03 03:31:49 +0100

    kein sudo für run

--- runTirex.sh
+++ runTirex.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Initialize PostgreSQL and Apache
-service tirex-backend-manager start
-service tirex-master start
+sudo -u tirex service tirex-backend-manager start
+sudo -u tirex service tirex-master start
+service apache2 start
+
 
 cd /usr/local/src/openstreetmap-carto || exit 1
 python3 editmapnikconfig.py
 
-tirex-master --debug
+sudo -u tirex tirex-master --debug
